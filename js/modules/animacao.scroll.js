8a63baa3d757302bc7b0137948be811d98371bb5
Jackson Lucas <jackson38582@gmail.com>
2023-05-13 19:27:20 -0300

    Anima-scroll retatorado para classe

--- js/modules/animacao.scroll.js
+++ js/modules/animacao.scroll.js
@@ -1,10 +1,15 @@
-export default function initScroll() {
-  // Use const em vez de let para variáveis que não são reatribuídas
+export default class Scroll {
+  constructor(selector) {
+    this.selector = document.querySelectorAll(selector);
+    this.observer = new IntersectionObserver(
+      this.handleIntersection.bind(this),
+      {
+        threshold: 1.0,
+      }
+    );
+  }
 
-  const items = document.querySelectorAll(".item");
-
-  // Extraia a função de callback do IntersectionObserver
-  function handleIntersection(entries, observer) {
+  handleIntersection(entries, observer) {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add("ativo");
@@ -12,17 +17,17 @@ export default function initScroll() {
     });
   }
 
-  // Crie o IntersectionObserver em uma constante
-  const observer = new IntersectionObserver(handleIntersection, {
-    threshold: 1.0,
-  });
+  eventoObserve() {
+    Array.from(this.selector).forEach((item) => {
+      const delay = parseInt(item.dataset.tempo);
+      setTimeout(() => {
+        this.observer.observe(item);
+      }, delay);
+    });
+  }
 
-  // Use Array.from() para converter o NodeList retornado por querySelectorAll em um array
-  // e use o método forEach() do array em vez do loop for
-  Array.from(items).forEach((item) => {
-    const delay = parseInt(item.dataset.tempo);
-    setTimeout(() => {
-      observer.observe(item);
-    }, delay);
-  });
+  init() {
+    this.eventoObserve();
+  }
 }
+
